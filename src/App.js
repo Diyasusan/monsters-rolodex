4c5876d534c0e03d0a7a310f63934b7a1ba2ab08
Diyasusan <diyasusan97@gmail.com>
2023-03-18 21:34:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import SearchBox from './components/search-box/search-box.component'
+import CardList from './components/card-list/card-list.component'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    //Constructor is always used to declare the state
+    this.state = {
+      monsters : [],
+      searchField : ''
+    }
+  }
+
+  componentDidMount(){
+    //after the componentDidMount change the state, render is called again to rernder the new value
+    //this is going to be a promise which is some asynchronous action in JS
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) =>
+      response.json())
+      .then((users)=>
+      this.setState(
+        //#1. Best Practice : If you are modifying any data in state, you want to keep your state to the original state.
+        ()=> {
+          return {monsters : users};
+        },
+        //now adding the callback method
+        ()=> {
+          console.log(this.state);
+        }
+      ))
+      //I can again have a .then on a promise
+           
+    }
+  onSearchChange = (event) => {
+      const searchField = event.target.value.toLocaleLowerCase();
+      this.setState(() => {
+        return {searchField};
+      });
+  }
+
+  render(){
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+      });
+
+    return (
+      <div className="App">
+        <SearchBox onChangeHandler={this.onSearchChange} placeholder = 'search-monsters' className = 'search-box'/>
+        <CardList monsters = {filteredMonsters}/>
+        </div>
+    );
+  }
+  
 }
 
 export default App;
